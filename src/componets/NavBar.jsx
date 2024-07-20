faf9f6a7e9faa6a7fb1c98f346d67b7ac57618d5
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-07-20 20:06:09 -0300

    Alterando o layout da página

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -1,20 +1,16 @@
+import { CgHome } from 'react-icons/cg';
 import './style/NavBar.css';
 
 function NavBar() {
-    const imagem = '../../vite.svg';
-
     return (
         <menu>
-            <img src={imagem} alt="Logo" className="logo" />
-
-            <a className='info' href="http://localhost:3000">Ínicio</a>
-            <a className='info' href="https://linkedin.com/in/Gustx21" target='_blank'>Tutoria online</a>
-            <a className='info' href="mailto://gugamathiaz12@gmail.com" target='_blank'>Contatos</a>
-
             <div>
-                <a href="http://localhost:3000/login"><button className='btn'>Crie sua conta</button></a>
-                <a href="http://localhost:3000/enter"><button className='btn'>Entrar</button></a>
+                <CgHome />
+                <a className='info' href="http://localhost:3000">Ínicio</a>
             </div>
+
+            <a href="http://localhost:3000/enter"><button className='btn'>Entrar</button></a>
+            <a href="http://localhost:3000/login"><button className='btn'>Crie sua conta</button></a>
         </menu>
     )
 }
