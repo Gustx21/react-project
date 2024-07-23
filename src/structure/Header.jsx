5430cb00b9a7afc3212b151af13b90346afc3965
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-07-23 18:55:34 -0300

    Adicionando texto legível

--- src/structure/Header.jsx
+++ src/structure/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header className="main">
             <h1>React Project</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, cupiditate quidem? Ipsa dolores porro at incidunt alias sit voluptatum, doloribus minima, rem sunt beatae ducimus sint nostrum, voluptatibus delectus. Architecto!</p>
+            <p>React é uma biblioteca JavaScript de código aberto para a construção de interfaces de usuário, mantida pelo Facebook e uma comunidade de desenvolvedores individuais e empresas. Foi criada por Jordan Walke, um engenheiro de software do Facebook, e lançada em 2013. React permite a construção de aplicações web de forma eficiente e estruturada, utilizando uma abordagem baseada em componentes.</p>
         </header>
     )
 }
