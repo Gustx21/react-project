62ebbc4217ca3558b35c1ddfb022e51b42ddee01
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-07-25 21:50:29 -0300

    Adicionando id direto no fragmento

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -8,21 +8,11 @@ import './styles/Home.css';
 function Home() {
     return (
         <div className="home" >
-            <div id="NavBar">
-                <NavBar />
-            </div>
-            <div id="Header">
-                <Header title={"React Project"} text={"React é uma biblioteca JavaScript de código aberto para a construção de interfaces de usuário, mantida pelo Facebook e uma comunidade de desenvolvedores individuais e empresas. Foi criada por Jordan Walke, um engenheiro de software do Facebook, e lançada em 2013. React permite a construção de aplicações web de forma eficiente e estruturada, utilizando uma abordagem baseada em componentes."}/>
-            </div>
-            <div id="Article">
-                <Article />
-            </div>
-            <div id="Aside">
-                <Aside />
-            </div>
-            <div id="Footer">
-                <Footer />
-            </div>
+            <NavBar />
+            <Header title={"React Project"} text={"React é uma biblioteca JavaScript de código aberto para a construção de interfaces de usuário, mantida pelo Facebook e uma comunidade de desenvolvedores individuais e empresas. Foi criada por Jordan Walke, um engenheiro de software do Facebook, e lançada em 2013. React permite a construção de aplicações web de forma eficiente e estruturada, utilizando uma abordagem baseada em componentes."} />
+            <Article />
+            <Aside />
+            <Footer />
         </div>
     )
 }
