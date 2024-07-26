33a21135a959c8110c24559feca10e659168c3e1
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-07-25 21:52:47 -0300

    Fragmentando o conteúdo

--- src/structure/Section.jsx
+++ src/structure/Section.jsx
@@ -1,25 +1,12 @@
-import { FaHtml5, FaNpm, FaReact, FaNodeJs } from "react-icons/fa";
-import './style/Section.css';
+import Card from "../../componets/Card";
+import Video from "../../componets/Video";
 
 function Section() {
     return (
-        <section className="areas">
-            <span>
-                <FaHtml5 />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ea expedita numquam maxime iste temporibus ad, minus voluptas hic, ullam laudantium iure repudiandae quis, amet aliquid eius. Obcaecati, a magnam.</p>
-            </span>
-            <span>
-                <FaNodeJs />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ea expedita numquam maxime iste temporibus ad, minus voluptas hic, ullam laudantium iure repudiandae quis, amet aliquid eius. Obcaecati, a magnam.</p>
-            </span>
-            <span>
-                <FaReact />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ea expedita numquam maxime iste temporibus ad, minus voluptas hic, ullam laudantium iure repudiandae quis, amet aliquid eius. Obcaecati, a magnam.</p>
-            </span>
-            <span>
-                <FaNpm />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ea expedita numquam maxime iste temporibus ad, minus voluptas hic, ullam laudantium iure repudiandae quis, amet aliquid eius. Obcaecati, a magnam.</p>
-            </span>
+        <section id="Section">
+            <Video />
+
+            <Card />
         </section>
     )
 }
