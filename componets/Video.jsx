cba87a71ea0e5be8ab4c23035f5108a439174fc1
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-07-25 21:57:35 -0300

    Criando fragments

---
+++ componets/Video.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Video() {
+    const youtubeID = '6IuQUgeDPg0';
+
+    return (
+      <iframe
+        className='video'
+        title='Youtube player'
+        sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
+        width={'100%'}
+        height={'400px'}
+        src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
+      ></iframe>
+    );
+}
+
+export default Video;
