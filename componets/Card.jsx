12f51ba92ea36b990315cd5f045510f1d7390a42
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-07-27 18:19:40 -0300

    Adicionando icon no card

--- componets/Card.jsx
+++ componets/Card.jsx
@@ -1,9 +1,9 @@
-import { FaHtml5, FaNpm, FaReact, FaNodeJs } from "react-icons/fa";
 import '../src/structure/style/Section.css';
 
-function Card({ title, text }) {
+function Card({ title, text, icon }) {
     return (
         <div className="cards">
+            {icon}
             <h4>{title}</h4>
             <p>{text}</p>
         </div>
--- src/structure/Section.jsx
+++ src/structure/Section.jsx
@@ -1,3 +1,5 @@
+
+import { FaHtml5, FaNpm, FaReact, FaNodeJs } from "react-icons/fa";
 import Card from "../../componets/Card";
 import Video from "../../componets/Video";
 
@@ -7,11 +9,13 @@ function Section() {
             <Video />
 
             <section className="areas">
-                <Card title={'Quantidade de linguagens'} text={'Estima-se que existam mais de 1150 linguagem de programação desenvolvidas até hoje. Cada uma foi projetada com propósitos específicos, e os desenvolvedores escolhem a mais adequada para cada projeto.'} />
-                <Card title={'Dia do programador'} text={'O Dia do Programador é comemorado em 12 de setembro. Uma data especial para homenagear aqueles que criam e mantêm o código que impulsiona a tecnologia e a inovação.'}/>
-                <Card title={'Programadores pedem ajuda'} text={'Curiosamente, programadores muitas vezes demoram para pedir ajuda quando enfrentam problemas. Isso pode ser atribuído ao formato de aprendizado, que incentiva a tentativa e erro.'}/>
+                <Card icon={<FaHtml5 />} title={'Quantidade de linguagens'} text={'Estima-se que existam mais de 1150 linguagem de programação desenvolvidas até hoje. Cada uma foi projetada com propósitos específicos, e os desenvolvedores escolhem a mais adequada para cada projeto.'} />
+
+                <Card icon={<FaNpm />} title={'Dia do programador'} text={'O Dia do Programador é comemorado em 12 de setembro. Uma data especial para homenagear aqueles que criam e mantêm o código que impulsiona a tecnologia e a inovação.'}/>
+
+                <Card icon={<FaReact />} title={'Programadores pedem ajuda'} text={'Curiosamente, programadores muitas vezes demoram para pedir ajuda quando enfrentam problemas. Isso pode ser atribuído ao formato de aprendizado, que incentiva a tentativa e erro.'}/>
                 
-                <Card title={'Diversidade de linguagens no Brasil'} text={'Embora programadores conheçam, em média, de 30 a 50 linguagens de programação, estima-se que mais de 1500 linguagens já tenham sido desenvolvidas.'}/>
+                <Card icon={<FaNodeJs />} title={'Diversidade de linguagens no Brasil'} text={'Embora programadores conheçam, em média, de 30 a 50 linguagens de programação, estima-se que mais de 1500 linguagens já tenham sido desenvolvidas.'}/>
             </section>
         </section>
     )
