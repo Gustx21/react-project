6b4ed450777ae7e91fabee693718c009180f4dbd
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-07-28 10:53:35 -0300

    Add função para capturar valores do input

--- pages/Enter.jsx
+++ pages/Enter.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdEmail } from 'react-icons/md';
 import { FaLock } from 'react-icons/fa';
 import './styles/Login.css';
 
 function Enter() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [checked, setCheckBox] = useState(false);
+
+    const handleQuery = (consulta) => {
+        consulta.preventDefault();
+
+        // const user = addUser();
+
+        window.location.href = 'http://localhost:3000';
+    }
+
     return (
         <div className="body">
-            <form className='login'>
+            <form className='login' onSubmit={handleQuery}>
                 <header>
                     <h3 className='titulo'>Sign In</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit magni consectetur aut excepturi error obcaecati.</p>
@@ -18,17 +30,17 @@ function Enter() {
 
                 <main className='form'>
                     <div>
-                        <input type="email" name="email" id="email" placeholder="E-mail" />
+                        <input type="email" name="email" id="email" placeholder="E-mail" value={email} onChange={(email) => setEmail(email.target.value)} />
                         <MdEmail />
                     </div>
                     <div>
-                        <input type="password" name="senha" id="senha" placeholder="Senha" />
+                        <input type="password" name="senha" id="senha" placeholder="Senha" value={senha} onChange={(senha) => setSenha(senha.target.value)} />
                         <FaLock />
                     </div>
 
                     <div>
                         <label>
-                            <input type="checkbox" />
+                            <input type="checkbox" checked={checked} onChange={(marked) => setCheckBox(marked.target.checked)} />
                             Lembre de mim
                         </label>
                     </div>
