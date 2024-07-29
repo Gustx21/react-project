d8fd81f0280841f646d52bf54484717f37d25efa
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-07-29 18:09:44 -0300

    Alterando os botões para links

--- src/structure/NavBar.jsx
+++ src/structure/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CgHome } from 'react-icons/cg';
 import './style/NavBar.css';
+import { BiDoorOpen, BiLogIn } from 'react-icons/bi';
 
 function NavBar() {
     return (
@@ -10,8 +11,15 @@ function NavBar() {
                 <a className='info' href="http://localhost:3000">Ínicio</a>
             </div>
 
-            <a href="http://localhost:3000/enter"><button className='btn'>Entrar</button></a>
-            <a href="http://localhost:3000/login"><button className='btn'>Crie sua conta</button></a>
+            <div>
+                <BiDoorOpen />
+                <a className='info' href="http://localhost:3000/enter">Entrar</a>
+            </div>
+
+            <div>
+                <BiLogIn />
+                <a className='info' href="http://localhost:3000/login">Crie sua conta</a>
+            </div>
         </menu>
     )
 }
