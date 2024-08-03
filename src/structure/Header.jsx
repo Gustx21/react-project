4ec070f322a6c5acb10a7caf2cd0bd89ca010be8
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-08-03 12:31:22 -0300

    Add

--- src/structure/Header.jsx
+++ src/structure/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header(props) {
     return (
-        <header className='Header'>
+        <header className='Header main'>
             <h1>{props.title}</h1>
             <p>{props.text}</p>
         </header>
