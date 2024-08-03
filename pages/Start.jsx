bff918aaf52f2479c10dc9893aec2b64d1a75404
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-08-03 17:11:49 -0300

    Adicionando leitor de arquivo .md

--- pages/Start.jsx
+++ pages/Start.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Markdown from 'react-markdown';
+import File from '../markdown/node.md';
 
-function Start() {
-    return (
-        <></>
-    )
+function MarkdownViewer() {
+  const [markdown, setMarkdown] = useState('');
+
+  useEffect(() => {
+    fetch(File)
+      .then((response) => response.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+
+  return <Markdown source={markdown} />;
 }
 
-export default Start;
+export default MarkdownViewer;
