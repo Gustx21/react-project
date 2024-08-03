ce8d3e28de4e1b911395993d1ea6b0a9f7709adb
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-08-03 09:31:21 -0300

    Excluindo as divs

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -8,13 +8,13 @@ import './styles/Home.css';
 
 function Home() {
     return (
-        <div className="home" >
+        <>
             <NavBar />
             <Header title={"React Project"} text={"React é uma biblioteca JavaScript de código aberto para a construção de interfaces de usuário, mantida pelo Facebook e uma comunidade de desenvolvedores individuais e empresas. Foi criada por Jordan Walke, um engenheiro de software do Facebook, e lançada em 2013. React permite a construção de aplicações web de forma eficiente e estruturada, utilizando uma abordagem baseada em componentes."} />
             <Article />
             <Aside />
             <Footer />
-        </div>
+        </>
     )
 }
 
