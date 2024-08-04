1698c352145a578e82a3e6679b5fbf120e9124be
Gustavo André⚡ <gugamathiaz12@gmail.com>
2024-08-04 11:49:57 -0300

    Add 'assetsInclude' para suportar arquivos .md

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.md'],
   server: {
     port: 3000,
     host: 'localhost'
